63ae2915573f81762225415285ee0e5296ca1da1
pasuvor3 <pasuvor3@mts.ru>
2025-02-22 22:52:51 +0300

    sprint-6/fix-1

--- cypress/e2e/burger-constructor.cy.js
+++ cypress/e2e/burger-constructor.cy.js
@@ -1,75 +1,91 @@
+/* eslint-disable cypress/no-unnecessary-waiting */
+const testUrl = "http://localhost:3000";
+
 describe("drag and drop tests", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3000");
+    cy.visit(testUrl);
     cy.intercept("GET", "https://norma.nomoreparties.space/api/ingredients", {
       fixture: "ingredients.json",
     });
   });
   it("should drag and drop bun", () => {
-    cy.get("[data-cy=ingredient]").contains("Булка 1").trigger("dragstart");
-    cy.get("[data-cy=constructor]").trigger("drop");
+    cy.get("[data-cy=ingredient]").as("ingredient");
+    cy.get("[data-cy=constructor]").as("constructor");
+    Cypress.Commands.add("drag", (name) => {
+      cy.get("@ingredient").contains(name).trigger("dragstart");
+    });
+    Cypress.Commands.add("drop", () => {
+      cy.get("@constructor").trigger("drop");
+    });
 
-    cy.get("[data-cy=constructor-bun-top]").should("contain", "Булка 1");
-    cy.get("[data-cy=constructor-bun-bottom]").should("contain", "Булка 1");
-    cy.get("[data-cy=ingredient]")
+    cy.wait(1000);
+    cy.drag("Булка 1");
+    cy.wait(1000);
+    cy.drop();
+
+    cy.get("[data-cy=constructor-bun-top]").as("constructor-bun-top");
+    cy.get("[data-cy=constructor-bun-bottom]").as("constructor-bun-bottom");
+
+    cy.get("@constructor-bun-top").should("contain", "Булка 1");
+    cy.get("@constructor-bun-bottom").should("contain", "Булка 1");
+    cy.get("@ingredient")
       .contains("Булка 1")
       .find("[data-cy=counter]")
       .should("contain", 2);
 
-    cy.get("[data-cy=ingredient]").contains("Булка 2").trigger("dragstart");
-    cy.get("[data-cy=constructor]").trigger("drop");
+    cy.drag("Булка 2");
+    cy.drop();
 
-    cy.get("[data-cy=constructor-bun-top]").should("contain", "Булка 2");
-    cy.get("[data-cy=constructor-bun-bottom]").should("contain", "Булка 2");
-    cy.get("[data-cy=ingredient]")
+    cy.get("@constructor-bun-top").should("contain", "Булка 2");
+    cy.get("@constructor-bun-bottom").should("contain", "Булка 2");
+    cy.get("@ingredient")
       .contains("Булка 2")
       .find("[data-cy=counter]")
       .should("contain", 2);
-    cy.get("[data-cy=ingredient]")
+    cy.get("@ingredient")
       .contains("Булка 1")
       .find("[data-cy=counter]")
       .should("not.exist");
   });
 
   it("should drag and drop ingredient", () => {
-    cy.get("[data-cy=ingredient]")
-      .contains("Ингредиент 1")
-      .trigger("dragstart");
-    cy.get("[data-cy=constructor]").trigger("drop");
+    cy.get("[data-cy=ingredient]").as("ingredient");
+    cy.get("[data-cy=constructor]").as("constructor");
+    Cypress.Commands.add("drag", (name) => {
+      cy.get("@ingredient").contains(name).trigger("dragstart");
+    });
+    Cypress.Commands.add("drop", () => {
+      cy.get("@constructor").trigger("drop");
+    });
 
-    cy.get("[data-cy=constructor-ingredient]")
-      .last()
-      .should("contain", "Ингредиент 1");
-    cy.get("[data-cy=constructor-ingredient]").should("have.length", 1);
-    cy.get("[data-cy=ingredient]")
+    cy.drag("Ингредиент 1");
+    cy.drop();
+
+    cy.get("[data-cy=constructor-ingredient]").as("constructor-ingredient");
+
+    cy.get("@constructor-ingredient").last().should("contain", "Ингредиент 1");
+    cy.get("@constructor-ingredient").should("have.length", 1);
+    cy.get("@ingredient")
       .contains("Ингредиент 1")
       .find("[data-cy=counter]")
       .should("contain", 1);
 
-    cy.get("[data-cy=ingredient]")
-      .contains("Ингредиент 2")
-      .trigger("dragstart");
-    cy.get("[data-cy=constructor]").trigger("drop");
+    cy.drag("Ингредиент 2");
+    cy.drop();
 
-    cy.get("[data-cy=constructor-ingredient]")
-      .last()
-      .should("contain", "Ингредиент 2");
-    cy.get("[data-cy=constructor-ingredient]").should("have.length", 2);
-    cy.get("[data-cy=ingredient]")
+    cy.get("@constructor-ingredient").last().should("contain", "Ингредиент 2");
+    cy.get("@constructor-ingredient").should("have.length", 2);
+    cy.get("@ingredient")
       .contains("Ингредиент 2")
       .find("[data-cy=counter]")
       .should("contain", 1);
 
-    cy.get("[data-cy=ingredient]")
-      .contains("Ингредиент 2")
-      .trigger("dragstart");
-    cy.get("[data-cy=constructor]").trigger("drop");
+    cy.drag("Ингредиент 2");
+    cy.drop();
 
-    cy.get("[data-cy=constructor-ingredient]")
-      .last()
-      .should("contain", "Ингредиент 2");
-    cy.get("[data-cy=constructor-ingredient]").should("have.length", 3);
-    cy.get("[data-cy=ingredient]")
+    cy.get("@constructor-ingredient").last().should("contain", "Ингредиент 2");
+    cy.get("@constructor-ingredient").should("have.length", 3);
+    cy.get("@ingredient")
       .contains("Ингредиент 2")
       .find("[data-cy=counter]")
       .should("contain", 2);
@@ -78,7 +94,7 @@ describe("drag and drop tests", () => {
 
 describe("ingredient modal tests", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3000");
+    cy.visit(testUrl);
     cy.intercept("GET", "https://norma.nomoreparties.space/api/ingredients", {
       fixture: "ingredients.json",
     });
@@ -91,21 +107,24 @@ describe("ingredient modal tests", () => {
   });
   it("should modal close by click button", () => {
     cy.get("[data-cy=ingredient]").contains("Булка 1").click();
-    cy.get("[data-cy=modal]").should("contain", "Детали ингредиента");
+    cy.get("[data-cy=modal]").as("modal");
+    cy.get("@modal").should("contain", "Детали ингредиента");
     cy.get("[data-cy=modal-close-icon]").click();
-    cy.get("[data-cy=modal]").should("not.exist");
+    cy.get("@modal").should("not.exist");
   });
   it("should modal close by click overlay", () => {
     cy.get("[data-cy=ingredient]").contains("Булка 1").click();
-    cy.get("[data-cy=modal]").should("contain", "Детали ингредиента");
+    cy.get("[data-cy=modal]").as("modal");
+    cy.get("@modal").should("contain", "Детали ингредиента");
     cy.get("[data-cy=modal-overlay]").click(0, 0);
-    cy.get("[data-cy=modal]").should("not.exist");
+    cy.get("@modal").should("not.exist");
   });
   it("should modal close by press key Escape", () => {
     cy.get("[data-cy=ingredient]").contains("Булка 1").click();
-    cy.get("[data-cy=modal]").should("contain", "Детали ингредиента");
+    cy.get("[data-cy=modal]").as("modal");
+    cy.get("@modal").should("contain", "Детали ингредиента");
     cy.get("body").type("{esc}");
-    cy.get("[data-cy=modal]").should("not.exist");
+    cy.get("@modal").should("not.exist");
   });
 });
 
@@ -120,7 +139,7 @@ describe("order create tests", () => {
     cy.intercept("POST", "https://norma.nomoreparties.space/api/orders", {
       fixture: "order.json",
     });
-    cy.visit("http://localhost:3000/login");
+    cy.visit(`${testUrl}/login`);
     cy.get("[data-cy=login-submit-button]").click();
   });
 
@@ -129,14 +148,21 @@ describe("order create tests", () => {
   });
 
   it("should create order", () => {
-    cy.get("[data-cy=ingredient]").contains("Булка 1").trigger("dragstart");
-    cy.get("[data-cy=constructor]").trigger("drop");
-    cy.get("[data-cy=ingredient]")
-      .contains("Ингредиент 1")
-      .trigger("dragstart");
-    cy.get("[data-cy=constructor]").trigger("drop");
-    cy.get("[data-cy=ingredient]").contains("Соус 1").trigger("dragstart");
-    cy.get("[data-cy=constructor]").trigger("drop");
+    cy.get("[data-cy=ingredient]").as("ingredient");
+    cy.get("[data-cy=constructor]").as("constructor");
+    Cypress.Commands.add("drag", (name) => {
+      cy.get("@ingredient").contains(name).trigger("dragstart");
+    });
+    Cypress.Commands.add("drop", () => {
+      cy.get("@constructor").trigger("drop");
+    });
+
+    cy.drag("Булка 1");
+    cy.drop();
+    cy.drag("Ингредиент 1");
+    cy.drop();
+    cy.drag("Соус 1");
+    cy.drop();
     cy.get("[data-cy=order-button]").click();
     cy.get("[data-cy=order-datails-number]").should("contain", 12345);
     cy.get("[data-cy=modal-close-icon]").click();
